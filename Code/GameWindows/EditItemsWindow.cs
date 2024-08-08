8d1004e0957bb7386f509df7e16e9e5916b1b53c
key44 <modding-contributions@keymasterer.uk>
2024-08-08 22:25:06 +0200

    did a scroll change that actually functions

--- Code/GameWindows/EditItemsWindow.cs
+++ Code/GameWindows/EditItemsWindow.cs
@@ -155,7 +155,7 @@ namespace PowerBox.Code.GameWindows {
 
       RectTransform rect = content.GetComponent<RectTransform>();
       rect.pivot = new Vector2(0, 1);
-      rect.sizeDelta = new Vector2(0, Mathf.Abs(GetPosByIndex(itemsList.Count + materials.Count).y) + 100);
+      rect.sizeDelta = new Vector2(0, Mathf.Abs(GetPosByIndex(itemsList.Where(item => !skipList.Contains(item.id)).SelectMany(item => item.materials.Select(material => (item, new ItemData() {id = item.id, material = material}))).Count(itemTuple => itemTuple.Item1.getSprite(itemTuple.Item2) != null)).y));
 
       foreach (ItemAsset item in itemsList.Where(item => !skipList.Contains(item.id))) {
         foreach (ItemAsset material in materials) {
