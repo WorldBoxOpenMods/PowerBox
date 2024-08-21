669b66db53d4c9225e141421e5daba74ddab191b
key44 <modding-contributions@keymasterer.uk>
2024-08-21 13:19:20 +0200

    added another TODO as a substitute for the lack of favorite item tracking

--- Code/PowerBox.cs
+++ Code/PowerBox.cs
@@ -17,12 +17,12 @@ using UnityEngine;
 // https://canary.discord.com/channels/522561390330904585/1232422556301000764/1234635618634436670
 // TODO: https://canary.discord.com/channels/522561390330904585/1232422556301000764/1241195995824390204
 // TODO: https://canary.discord.com/channels/522561390330904585/1232422556301000764/1241282748132622357
+// TODO: window for viewing every creature in a world
 
 // Later version TODOs
 // TODO: make favorite items persistent, see https://canary.discord.com/channels/@me/1188525491297194044/1235623580402978886 for method
 // TODO: refactor GameWindows/EditItemsWindow.cs
 // TODO: https://canary.discord.com/channels/522561390330904585/1232422556301000764/1236953743434448906
-// TODO: window for viewing every creature in a world
 
 namespace PowerBox.Code {
   public class PowerBox : BasicMod<PowerBox> {
