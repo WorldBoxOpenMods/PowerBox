0b4e8126903b257108a5a7115517302a9f0bf53a
key44 <modding-contributions@keymasterer.uk>
2024-08-30 12:56:38 +0200

    modified culture tech editor sizing to be relative based on screen resolution

--- Code/Features/Windows/EditCultureTechWindow.cs
+++ Code/Features/Windows/EditCultureTechWindow.cs
@@ -88,9 +88,9 @@ namespace PowerBox.Code.Features.Windows {
       GameObject buttonLabel = new GameObject($"{tech.id} TechButtonLabel", typeof(Text));
       buttonLabel.GetComponent<Text>().text = LM.Get($"tech_{tech.id}");
       buttonLabel.GetComponent<Text>().font = techButton.GetComponentInChildren<Text>().font;
-      buttonLabel.GetComponent<Text>().fontSize = 60;
-      buttonLabel.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
-      buttonLabel.GetComponent<RectTransform>().sizeDelta = new Vector2(buttonLabel.GetComponent<RectTransform>().sizeDelta.x + 850.0f, buttonLabel.GetComponent<RectTransform>().sizeDelta.y);
+      buttonLabel.GetComponent<Text>().fontSize = Screen.height / 40;
+      buttonLabel.GetComponent<Text>().alignment = TextAnchor.MiddleRight;
+      buttonLabel.GetComponent<RectTransform>().sizeDelta = new Vector2(buttonLabel.GetComponent<RectTransform>().sizeDelta.x + Screen.width / 7.5f, buttonLabel.GetComponent<RectTransform>().sizeDelta.y);
       GameObject buttonImage = new GameObject($"{tech.id} TechButtonImage", typeof(Image));
       buttonImage.GetComponent<Image>().sprite = SpriteTextureLoader.getSprite("ui/Icons/" + tech.path_icon);
       buttonImage.transform.SetParent(parent);
