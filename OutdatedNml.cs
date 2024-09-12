bb524aa94026bcbccf8292fe3ccf35616cefce04
key44 <modding-contributions@keymasterer.uk>
2024-09-12 19:18:16 +0200

    also forgot GameObject get implementation

--- OutdatedNml.cs
+++ OutdatedNml.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 namespace PowerBox {
   public class OutdatedNml : MonoBehaviour, IMod {
     private ModDeclare _modDeclare;
+    private GameObject _gameObject;
     public ModDeclare GetDeclaration() {
       return _modDeclare;
     }
     public GameObject GetGameObject() {
-      throw new System.NotImplementedException();
+      return _gameObject;
     }
     public string GetUrl() {
       return "https://github.com/WorldBoxOpenMods/PowerBox";
     }
     public void OnLoad(ModDeclare pModDecl, GameObject pGameObject) {
       _modDeclare = pModDecl;
+      _gameObject = pGameObject;
       Debug.LogError("You're using an outdated NML version that doesn't support precompiled mods. Please get version 1.0.9 of NeoModLoader or later.");
     }
   }
