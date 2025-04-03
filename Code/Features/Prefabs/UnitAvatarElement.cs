71c7d3f37fa0cc0e628a9171c9c59343b3b3dcbe
key44 <modding-contributions@keymasterer.uk>
2025-04-03 10:09:42 +0200

    forgot a starting slash

--- Code/Features/Prefabs/UnitAvatarElement.cs
+++ Code/Features/Prefabs/UnitAvatarElement.cs
@@ -4,7 +4,7 @@ namespace PowerBox.Code.Features.Prefabs {
   public class UnitAvatarElement : ModObjectFeature<GameObject> {
     public GameObject Prefab => Object;
     protected override GameObject InitObject() {
-      return GameObject.Find("Canvas Container Main/Canvas - UI/General/CanvasBottom/BottomElements/BottomElementsMover/CanvasScrollView/Scroll View/Viewport/Content/buttons/Tab_SelectedUnit/actor_info/UnitAvatarElement");
+      return GameObject.Find("/Canvas Container Main/Canvas - UI/General/CanvasBottom/BottomElements/BottomElementsMover/CanvasScrollView/Scroll View/Viewport/Content/buttons/Tab_SelectedUnit/actor_info/UnitAvatarElement");
     }
   }
 }
