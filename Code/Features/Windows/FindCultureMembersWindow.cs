e771912ef0dc4afa0a4e7b66ef093449341e92d1
key44 <modding-contributions@keymasterer.uk>
2025-04-04 08:50:31 +0200

    added button creation functionality used in FindAllCreaturesWindow to FindCultureMembersWindow

--- Code/Features/Windows/FindCultureMembersWindow.cs
+++ Code/Features/Windows/FindCultureMembersWindow.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using HarmonyLib;
 using NeoModLoader.api;
 using NeoModLoader.General;
+using PowerBox.Code.Features.Patches;
+using PowerBox.Code.Features.UiHelpers;
 using PowerBox.Code.Scheduling;
 using PowerBox.Code.Utils;
 using UnityEngine;
@@ -11,7 +13,7 @@ using UnityEngine.UI;
 
 namespace PowerBox.Code.Features.Windows {
   public class FindCultureMembersWindow : WindowBase<FindCultureMembersWindow> {
-    public override ModFeatureRequirementList RequiredModFeatures => base.RequiredModFeatures + typeof(Harmony);
+    public override ModFeatureRequirementList RequiredModFeatures => base.RequiredModFeatures + typeof(Harmony) + typeof(UnitButtonCreator) + typeof(PreventSettingActorToNullInUiUnitAvatarOnDisablePatch);
 
     protected override ScrollWindow InitObject() {
       ScrollWindow window = WindowCreator.CreateEmptyWindow("powerbox_find_culture_members", "powerbox_find_culture_members");
@@ -58,6 +60,7 @@ namespace PowerBox.Code.Features.Windows {
         selectedCulture = Config.selected_culture;
       }
       if (selectedCulture is null) {
+        PowerBox.LogWarning("InitFindCultureMembers() called with no culture selected");
         return;
       }
       RectTransform rt = SpriteHighlighter.GetComponent<RectTransform>();
@@ -71,9 +74,8 @@ namespace PowerBox.Code.Features.Windows {
       uiActions.Add(window.resetScroll);
       uiActions.Add(content.transform.DetachChildren);
       uiActions.Add(window.resetScroll);
-      uiActions.AddRange(cultureFollowers.Select((t, i) => i).
-        Select(index => (Action)(() => {
-          CreateFollowerButton(cultureFollowers, index, content);
+      uiActions.AddRange(cultureFollowers.Select((unit, index) => (Action)(() => {
+          GetFeature<UnitButtonCreator>().CreateUnitButton(unit, index, content);
           if (index % 5 == 0) {
             // ReSharper disable once PossibleLossOfFraction
             content.GetComponent<RectTransform>().sizeDelta = new Vector2(0.0f, 40.0f * (index / 5 + 1));
@@ -88,49 +90,6 @@ namespace PowerBox.Code.Features.Windows {
         uiActions.Add(onFinishAction);
       }
       Scheduler.Instance.Schedule(new Schedule(10, uiActions.ToArray()));
-    }
-    private static void CreateFollowerButton(IReadOnlyList<Actor> cultureFollowers, int index, GameObject content) {
-      Actor follower = cultureFollowers[index];
-      GameObject followerInfo = new GameObject {
-        name = "follower_" + index + "_info",
-        transform = {
-          parent = content.transform,
-          // ReSharper disable once PossibleLossOfFraction
-          localPosition = new Vector3(50.0f + 40 * (index % 5), -20.0f + (index / 5) * -40.0f, 0.0f),
-          localScale = new Vector3(0.9f, 0.9f, 0.9f)
-        }
-      };
-      GameObject followerInfoChild = PowerButtonCreator.CreateSimpleButton("follower_" + index, () => {
-        if (follower is null || follower.isAlive() == false || World.world.units.getSimpleList().Contains(follower) == false) {
-          return;
-        }
-        SelectedUnit.select(follower);
-        ScrollWindow.showWindow("inspect_unit");
-      }, null, followerInfo.transform).gameObject;
-      followerInfoChild.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
-      UiUnitAvatarElement creatureInfo = followerInfoChild.AddComponent<UiUnitAvatarElement>();
-      GameObject garbage = new GameObject();
-      Image unitTypeBg = garbage.GetComponent<Image>();
-      creatureInfo.unit_type_bg = unitTypeBg ?? garbage.AddComponent<Image>();
-      creatureInfo.avatarLoader = creatureInfo.gameObject.AddComponent<UnitAvatarLoader>();
-      creatureInfo.show_banner_kingdom = false;
-      creatureInfo.show_banner_clan = false;
-      creatureInfo.Start();
-      creatureInfo.show(follower);
-      creatureInfo.unit_type_bg = null;
-      UnityEngine.Object.Destroy(garbage);
-      followerInfoChild.transform.GetChild(0).name = "follower_" + index + "_avatar";
-      followerInfoChild.transform.GetChild(0).localPosition = new Vector3(0.0f, -10.0f, 0.0f);
-      if (follower.asset.is_boat) {
-        followerInfo.transform.GetChild(0).localScale = new Vector3(1.5f, 1.5f, 1.5f);
-      } else {
-        followerInfoChild.transform.GetChild(0).localScale = new Vector3(2.2f, 2.2f, 2.2f);
-      }
-      for (int j = 1; j < followerInfoChild.transform.childCount; j++) {
-        UnityEngine.Object.Destroy(followerInfoChild.transform.GetChild(j).gameObject);
-      }
-      LM.AddToCurrentLocale(followerInfoChild.name, follower.coloredName);
-      creatureInfo.gameObject.SetActive(true);
     }
   }
 }
