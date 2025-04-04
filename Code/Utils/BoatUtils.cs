1f68036273b6fa38bad9ce6f4c589586c320e911
key44 <modding-contributions@keymasterer.uk>
2025-04-04 10:15:47 +0200

    fixed bug that was causing boat spawns to break

--- Code/Utils/BoatUtils.cs
+++ Code/Utils/BoatUtils.cs
@@ -1,11 +1,14 @@
 namespace PowerBox.Code.Utils {
   public class BoatUtils {
     public static void BoatSpawnAction(WorldTile pTile, string boatType) {
-      if (pTile.zone.city == null)
-        return;
+      if (pTile.zone.city == null) return;
+      if (!pTile.Type.liquid) return;
 
-      if (!pTile.Type.liquid)
-        return;
+      if (boatType != "boat_fishing") {
+        Subspecies subspecies = pTile.zone.city.getMainSubspecies();
+        if (subspecies == null) return;
+        boatType += $"_{subspecies.species_id}";
+      }
 
       Actor actor = MapBox.instance.units.spawnNewUnit(boatType, pTile);
 
