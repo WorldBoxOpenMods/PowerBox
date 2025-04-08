a3d5216ce62cc2f6d34d90b9fa9ebcd85d71a15b
key44 <modding-contributions@keymasterer.uk>
2025-04-08 06:32:16 +0200

    changed PowerBox build script to bundle code instead of DLL

--- assemble_mod_distributable.sh
+++ assemble_mod_distributable.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Init PowerBox related path variables
-POWERBOX_BIN_PATH=~/RiderProjects/Modding/KeyMods/PowerBox/bin/Release
+POWERBOX_PROJECT_PATH=~/RiderProjects/Modding/KeyMods/PowerBox
 NML_MODS_FOLDER_PATH=~/Library/Application\ Support/Steam/steamapps/common/worldbox/Mods
 
 # Init PowerBox distributable folder structure
@@ -10,20 +10,15 @@ rm -rf "./PowerBox"
 rm -rf "./KEYMASTERER__DON_NIKON_POWERBOX" # mod folder that NML auto generates if PowerBox.zip is unzipped by it
 mkdir "./PowerBox"
 
-# Copy built files into distributable folder
-cd "$POWERBOX_BIN_PATH" || exit
-mv "./PowerBox.dll" "$NML_MODS_FOLDER_PATH/PowerBox/PowerBox.dll"
-mv "./PowerBox.pdb" "$NML_MODS_FOLDER_PATH/PowerBox/PowerBox.pdb"
-
 # Copy build assets into distributable folder
-cd "../../" || exit
+cd "$POWERBOX_PROJECT_PATH" || exit
+cp -R "./Code" "$NML_MODS_FOLDER_PATH/PowerBox/"
 cp -R "./Assemblies" "$NML_MODS_FOLDER_PATH/PowerBox/"
 cp -R "./EmbeddedResources" "$NML_MODS_FOLDER_PATH/PowerBox/"
 cp -R "./GameResources" "$NML_MODS_FOLDER_PATH/PowerBox/"
 cp -R "./Locales" "$NML_MODS_FOLDER_PATH/PowerBox/"
 cp -R "./icon.png" "$NML_MODS_FOLDER_PATH/PowerBox/icon.png"
 cp -R "./mod.json" "$NML_MODS_FOLDER_PATH/PowerBox/mod.json"
-cp -R "./OutdatedNml.cs" "$NML_MODS_FOLDER_PATH/PowerBox/OutdatedNml.cs"
 
 # Compress distributable folder into zip file
 cd "$NML_MODS_FOLDER_PATH" || exit
