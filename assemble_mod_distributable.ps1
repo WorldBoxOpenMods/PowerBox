b1b475a5e5e66212c7efdb20291fc078d3f66a4f
key44 <modding-contributions@keymasterer.uk>
2025-04-20 12:41:41 +0200

    updated assemble_mod_distributable scripts to be able to support binary distribution

--- assemble_mod_distributable.ps1
+++ assemble_mod_distributable.ps1
@@ -1,74 +1,72 @@
-# PowerShell equivalent of assemble_mod_distributable.sh
-
-# Define basic mod info
+# Define basic info about mod to distribute
 $MOD_NAME = "PowerBox"
 $NML_AUTO_UNZIP_OUTPUT_FOLDER_NAME = "KEYMASTERER__DON_NIKON_POWERBOX"
+$DISTRIBUTION_KIND = "source"  # "source" or "binary"
 
-# Handle arguments and set paths
-param(
-[string]$ModProjectPath,
-[string]$NmlModsFolderPath,
-[string]$DistributableArchivePath
-)
-
-function Show-Help {
-    Write-Host "Usage: .\assemble_mod_distributable.ps1 [-ModProjectPath <path>] [-NmlModsFolderPath <path>] [-DistributableArchivePath <path>]"
+# Init mod related path variables
+if ($args.Count -eq 0) {
+    Write-Host "No arguments supplied, using default paths. To see available paths, run with -h or --help."
+    $MOD_PROJECT_PATH = "$env:USERPROFILE\RiderProjects\Modding\KeyMods\$MOD_NAME"
+    $MOD_BINARY_PATH = "$MOD_PROJECT_PATH\bin\Release"
+    $NML_MODS_FOLDER_PATH = "$env:USERPROFILE\Library\Application Support\Steam\steamapps\common\worldbox\Mods"
+    $DISTRIBUTABLE_ARCHIVE_PATH = "$NML_MODS_FOLDER_PATH\$MOD_NAME.zip"
+} elseif ($args[0] -eq "-h" -or $args[0] -eq "--help") {
+    Write-Host "Usage: .\assemble_mod_distributable.ps1 [ModProjectPath] [ModBinaryPath] [NMLModsFolderPath] [(optional) DistributableArchivePath]"
     Write-Host "If no arguments are supplied, default paths will be used."
     exit 0
+} else {
+    $MOD_PROJECT_PATH = $args[0]
+    $MOD_BINARY_PATH = $args[1]
+    $NML_MODS_FOLDER_PATH = $args[2]
+    if ($args.Count -eq 4) {
+        $DISTRIBUTABLE_ARCHIVE_PATH = $args[3]
+    } else {
+        Write-Host "Distributable archive path not supplied, using default path."
+        $DISTRIBUTABLE_ARCHIVE_PATH = "$NML_MODS_FOLDER_PATH\$MOD_NAME.zip"
+    }
 }
 
-if ($args -contains "-h" -or $args -contains "--help") {
-    Show-Help
+# Init mod distributable folder structure
+Set-Location -Path $NML_MODS_FOLDER_PATH
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$MOD_NAME"
+if (Test-Path "$NML_AUTO_UNZIP_OUTPUT_FOLDER_NAME") {
+    Remove-Item -Recurse -Force "$NML_AUTO_UNZIP_OUTPUT_FOLDER_NAME"
+}
+New-Item -ItemType Directory -Path "$MOD_NAME" | Out-Null
+
+if ($DISTRIBUTION_KIND -eq "binary") {
+    Set-Location -Path $MOD_BINARY_PATH
+    Copy-Item "$MOD_NAME.dll" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\$MOD_NAME.dll"
+    Copy-Item "$MOD_NAME.pdb" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\$MOD_NAME.pdb"
 }
 
-if (-not $ModProjectPath) {
-    Write-Host "No arguments supplied, using default paths. To see available paths, run with -h or --help."
-    $ModProjectPath = "$HOME\RiderProjects\Modding\KeyMods\$MOD_NAME"
-    $NmlModsFolderPath = "$HOME\AppData\Roaming\Steam\steamapps\common\worldbox\Mods"
-    $DistributableArchivePath = Join-Path $NmlModsFolderPath "$MOD_NAME.zip"
-} elseif (-not $DistributableArchivePath) {
-    Write-Host "Distributable archive path not supplied, using default path."
-    $DistributableArchivePath = Join-Path $NmlModsFolderPath "$MOD_NAME.zip"
+# Copy build assets into distributable folder
+Set-Location -Path $MOD_PROJECT_PATH
+
+if ($DISTRIBUTION_KIND -eq "source") {
+    Copy-Item "Code" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\" -Recurse
+} elseif ($DISTRIBUTION_KIND -eq "binary") {
+    if (Test-Path "OutdatedNml.cs") {
+        Copy-Item "OutdatedNml.cs" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\"
+    }
 }
-
-# Prepare mod distributable folder
-Set-Location $NmlModsFolderPath
-
-$modFolderPath = Join-Path $NmlModsFolderPath $MOD_NAME
-$autoUnzipPath = Join-Path $NmlModsFolderPath $NML_AUTO_UNZIP_OUTPUT_FOLDER_NAME
-
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue $modFolderPath
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue $autoUnzipPath
-
-New-Item -ItemType Directory -Path $modFolderPath | Out-Null
-
-# Copy build assets
-Set-Location $ModProjectPath
-
-Copy-Item -Recurse -Force "./Code" $modFolderPath
-
-if (Test-Path "./Assemblies") {
-    Copy-Item -Recurse -Force "./Assemblies" $modFolderPath
+if (Test-Path "Assemblies") {
+    Copy-Item "Assemblies" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\" -Recurse
 }
-if (Test-Path "./EmbeddedResources") {
-    Copy-Item -Recurse -Force "./EmbeddedResources" $modFolderPath
+if (Test-Path "EmbeddedResources") {
+    Copy-Item "EmbeddedResources" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\" -Recurse
 }
-if (Test-Path "./GameResources") {
-    Copy-Item -Recurse -Force "./GameResources" $modFolderPath
+if (Test-Path "GameResources") {
+    Copy-Item "GameResources" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\" -Recurse
 }
+Copy-Item "Locales" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\" -Recurse
+Copy-Item "icon.png" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\icon.png"
+Copy-Item "mod.json" -Destination "$NML_MODS_FOLDER_PATH\$MOD_NAME\mod.json"
 
-Copy-Item -Force "./Locales" $modFolderPath -Recurse
-Copy-Item -Force "./icon.png" "$modFolderPath\icon.png"
-Copy-Item -Force "./mod.json" "$modFolderPath\mod.json"
-
-# Create distributable zip
-Set-Location $NmlModsFolderPath
-
-if (Test-Path $DistributableArchivePath) {
-    Remove-Item -Force $DistributableArchivePath
+# Compress distributable folder into zip file
+Set-Location -Path $NML_MODS_FOLDER_PATH
+if (Test-Path $DISTRIBUTABLE_ARCHIVE_PATH) {
+    Remove-Item $DISTRIBUTABLE_ARCHIVE_PATH -Force
 }
-
-Add-Type -AssemblyName 'System.IO.Compression.FileSystem'
-[System.IO.Compression.ZipFile]::CreateFromDirectory($modFolderPath, "$NmlModsFolderPath\$MOD_NAME.zip")
-
-Move-Item -Force "$NmlModsFolderPath\$MOD_NAME.zip" $DistributableArchivePath
+Compress-Archive -Path "$MOD_NAME\*" -DestinationPath "./$MOD_NAME.zip"
+Move-Item -Path "./$MOD_NAME.zip" -Destination $DISTRIBUTABLE_ARCHIVE_PATH -Force
--- assemble_mod_distributable.sh
+++ assemble_mod_distributable.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 # Define basic info about mod to distribute
-MOD_NAME="PowerBox"
-NML_AUTO_UNZIP_OUTPUT_FOLDER_NAME="KEYMASTERER__DON_NIKON_POWERBOX"
+MOD_NAME="PowerBox" # mod name, must be what the mod DLL is named if you're using binary distribution, otherwise can be whatever you want the mod folder/zip to be called
+NML_AUTO_UNZIP_OUTPUT_FOLDER_NAME="KEYMASTERER__DON_NIKON_POWERBOX" # if the mod.zip is placed in the NML Mods folder, NML will automatically unzip it upon opening the name with an auto generated folder name, specify said name here
+DISTRIBUTION_KIND="source" # "source" or "binary", depending on whether mod should be distributed as source code of as DLL/PDB files
 
 # Init mod related path variables
 if test $# -eq 0; then
   echo "No arguments supplied, using default paths. To see available paths, run with -h or --help."
-  MOD_PROJECT_PATH=~/RiderProjects/Modding/KeyMods/$MOD_NAME
-  NML_MODS_FOLDER_PATH=~/Library/Application\ Support/Steam/steamapps/common/worldbox/Mods
+  MOD_PROJECT_PATH="$HOME/RiderProjects/Modding/KeyMods/$MOD_NAME"
+  MOD_BINARY_PATH="$MOD_PROJECT_PATH/bin/Release"
+  NML_MODS_FOLDER_PATH="$HOME/Library/Application Support/Steam/steamapps/common/worldbox/Mods"
   DISTRIBUTABLE_ARCHIVE_PATH="$NML_MODS_FOLDER_PATH/$MOD_NAME.zip"
 else
   if test "$1" = "-h" || test "$1" = "--help"; then
-    echo "Usage: ./assemble_mod_distributable.sh [ModProjectPath] [NMLModsFolderPath] [(optional) DistributableArchivePath]"
+    echo "Usage: ./assemble_mod_distributable.sh [ModProjectPath] [ModBinaryPath] [NMLModsFolderPath] [(optional) DistributableArchivePath]"
     echo "If no arguments are supplied, default paths will be used."
     exit 0
   else
     MOD_PROJECT_PATH=$1
-    NML_MODS_FOLDER_PATH=$2
-    if test $# -eq 3; then
-      DISTRIBUTABLE_ARCHIVE_PATH=$3
+    MOD_BINARY_PATH=$2
+    NML_MODS_FOLDER_PATH=$3
+    if test $# -eq 4; then
+      DISTRIBUTABLE_ARCHIVE_PATH=$4
     else
       echo "Distributable archive path not supplied, using default path."
       DISTRIBUTABLE_ARCHIVE_PATH="$NML_MODS_FOLDER_PATH/$MOD_NAME.zip"
@@ -34,9 +37,22 @@ if test -d "./$NML_AUTO_UNZIP_OUTPUT_FOLDER_NAME"; then
 fi
 mkdir "./$MOD_NAME"
 
+if test "$DISTRIBUTION_KIND" = "binary"; then
+  # Copy built files into distributable folder
+  cd "$MOD_BINARY_PATH" || exit 1
+  cp -R "./$MOD_NAME.dll" "$NML_MODS_FOLDER_PATH/$MOD_NAME/$MOD_NAME.dll"
+  cp -R "./$MOD_NAME.pdb" "$NML_MODS_FOLDER_PATH/$MOD_NAME/$MOD_NAME.pdb"
+fi
+
 # Copy build assets into distributable folder
 cd "$MOD_PROJECT_PATH" || exit 1
-cp -R "./Code" "$NML_MODS_FOLDER_PATH/$MOD_NAME/"
+if test "$DISTRIBUTION_KIND" = "source"; then
+  cp -R "./Code" "$NML_MODS_FOLDER_PATH/$MOD_NAME/"
+elif test "$DISTRIBUTION_KIND" = "binary"; then
+  if test -e "./OutdatedNml.cs"; then
+    cp -R "./OutdatedNml.cs" "$NML_MODS_FOLDER_PATH/$MOD_NAME/OutdatedNml.cs"
+  fi
+fi
 if test -d "./Assemblies"; then
   cp -R "./Assemblies" "$NML_MODS_FOLDER_PATH/$MOD_NAME/"
 fi
