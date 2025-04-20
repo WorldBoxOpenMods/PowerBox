64381017037318a98864541e2be09979adad79b5
key44 <k.minihuber@students.htl-leonding.ac.at>
2025-04-20 15:46:16 +0200

    fixed a bug that was breaking param handling on the powershell version of the dist script

--- assemble_mod_distributable.ps1
+++ assemble_mod_distributable.ps1
@@ -1,29 +1,56 @@
+param (
+    [string]$ModProjectPath,
+    [string]$ModBinaryPath,
+    [string]$NmlModsFolderPath,
+    [string]$DistributableArchivePath
+)
+
 # Define basic info about mod to distribute
 $MOD_NAME = "PowerBox"
 $NML_AUTO_UNZIP_OUTPUT_FOLDER_NAME = "KEYMASTERER__DON_NIKON_POWERBOX"
 $DISTRIBUTION_KIND = "source"  # "source" or "binary"
 
 # Init mod related path variables
-if ($args.Count -eq 0) {
-    Write-Host "No arguments supplied, using default paths. To see available paths, run with -h or --help."
+if (-not $ModProjectPath) {
+    Write-Host "No mod project path provided, using default path. To see available paths, run with -h or --help."
     $MOD_PROJECT_PATH = "$env:USERPROFILE\RiderProjects\Modding\KeyMods\$MOD_NAME"
+} else {
+    $MOD_PROJECT_PATH = $ModProjectPath
+}
+if (-not $ModBinaryPath) {
+    Write-Host "No mod project path provided, using default path. To see available paths, run with -h or --help."
     $MOD_BINARY_PATH = "$MOD_PROJECT_PATH\bin\Release"
+} else {
+    $MOD_BINARY_PATH = $ModBinaryPath
+}
+if (-not $NmlModsFolderPath) {
+    Write-Host "No mod project path provided, using default path. To see available paths, run with -h or --help."
     $NML_MODS_FOLDER_PATH = "C:\Program Files (x86)\Steam\steamapps\common\worldbox\Mods"
+} else {
+    $NML_MODS_FOLDER_PATH = $NmlModsFolderPath
+}
+if (-not $DistributableArchivePath) {
+    Write-Host "No distributable archive path provided, using default path. To see available paths, run with -h or --help."
     $DISTRIBUTABLE_ARCHIVE_PATH = "$NML_MODS_FOLDER_PATH\$MOD_NAME.zip"
-} elseif ($args[0] -eq "-h" -or $args[0] -eq "--help") {
+} else {
+    $DISTRIBUTABLE_ARCHIVE_PATH = $DistributableArchivePath
+}
+if ($args[0] -eq "-h" -or $args[0] -eq "--help") {
     Write-Host "Usage: .\assemble_mod_distributable.ps1 [ModProjectPath] [ModBinaryPath] [NMLModsFolderPath] [(optional) DistributableArchivePath]"
     Write-Host "If no arguments are supplied, default paths will be used."
     exit 0
-} else {
-    $MOD_PROJECT_PATH = $args[0]
-    $MOD_BINARY_PATH = $args[1]
-    $NML_MODS_FOLDER_PATH = $args[2]
-    if ($args.Count -eq 4) {
-        $DISTRIBUTABLE_ARCHIVE_PATH = $args[3]
-    } else {
-        Write-Host "Distributable archive path not supplied, using default path."
-        $DISTRIBUTABLE_ARCHIVE_PATH = "$NML_MODS_FOLDER_PATH\$MOD_NAME.zip"
-    }
+}
+
+# Print out paths for debugging
+Write-Host "Mod Project Path: $MOD_PROJECT_PATH"
+Write-Host "Mod Binary Path: $MOD_BINARY_PATH"
+Write-Host "Nml Mods Folder Path: $NML_MODS_FOLDER_PATH"
+Write-Host "Distributable Archive Path: $DISTRIBUTABLE_ARCHIVE_PATH"
+
+# Check if NML_MODS_FOLDER_PATH is correct (is absolute path)
+if (-not (Test-Path $NML_MODS_FOLDER_PATH)) {
+    Write-Host "The NML Mods folder path is incorrect or doesn't exist."
+    exit 1
 }
 
 # Init mod distributable folder structure
@@ -68,5 +95,5 @@ Set-Location -Path $NML_MODS_FOLDER_PATH
 if (Test-Path $DISTRIBUTABLE_ARCHIVE_PATH) {
     Remove-Item $DISTRIBUTABLE_ARCHIVE_PATH -Force
 }
-Compress-Archive -Path "$MOD_NAME\*" -DestinationPath "./$MOD_NAME.zip"
-Move-Item -Path "./$MOD_NAME.zip" -Destination $DISTRIBUTABLE_ARCHIVE_PATH -Force
+Compress-Archive -Path "$MOD_NAME\*" -DestinationPath "$DISTRIBUTABLE_ARCHIVE_PATH"
+Write-Host "Archive created at $DISTRIBUTABLE_ARCHIVE_PATH"
