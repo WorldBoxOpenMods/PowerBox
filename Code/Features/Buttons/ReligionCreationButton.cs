13616344820ea92e8cc4345669e23cc0c929cdc8
IFailedLife3064 <ifailedlife@keymasterer.uk>
2025-08-10 21:33:47 +0200

    added religion creation power

---
+++ Code/Features/Buttons/ReligionCreationButton.cs
@@ -0,0 +1,10 @@
+using PowerBox.Code.Features.GodPowers;
+using NeoModLoader.api;
+using NeoModLoader.api.features;
+
+namespace PowerBox.Code.Features.Buttons {
+  public class ReligionCreationButton : ModGodPowerButtonFeature<ReligionCreationPower, Tab> {
+    public override ModFeatureRequirementList OptionalModFeatures => typeof(CultureAdditionButton);
+    public override string SpritePath => "ui/icons/iconreligion";
+  }
+}
---
+++ Code/Features/GodPowers/ReligionCreationPower.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using NeoModLoader.api.features;
+
+namespace PowerBox.Code.Features.GodPowers {
+  public class ReligionCreationPower : ModAssetFeature<GodPower> {
+    protected override GodPower InitObject() {
+      return new GodPower {
+        id = "powerbox_create_new_religion",
+        name = "powerbox_create_new_religion",
+        force_brush = "circ_0",
+        falling_chance = 0.03f,
+        hold_action = false,
+        show_tool_sizes = false,
+        unselect_when_window = true,
+        click_special_action = ReligionCreationAction
+      };
+    }
+
+    private static bool ReligionCreationAction(WorldTile pTile = null, string pPowerId = null) {
+      if (pTile?.zone?.city == null) return false;
+      City targetCity = pTile.zone.city;
+      Actor targetUnit = targetCity.hasLeader() ? targetCity.leader : targetCity.units.FirstOrDefault();
+      if (targetUnit == null) return false;
+      Religion newReligion = World.world.religions.newReligion(targetUnit, true);
+      foreach (Actor actor in targetCity.units) {
+        actor.setReligion(newReligion);
+      }
+      return true;
+    }
+  }
+}
